b9ce393d28ed20ce31b1de1d3c8d7f3a4cb5bf9b
liuyumao <PinelliaSadness@Gmail.com>
2019-10-31 18:11:06 +0800

    init

--- netty/src/main/java/com/summersadness/netty/discard/DiscardServer.java
+++ netty/src/main/java/com/summersadness/netty/discard/DiscardServer.java
@@ -55,7 +55,7 @@ public class DiscardServer {
     public static void main(String[] args) throws Exception{
         int port = 8080;
         if (args.length > 0) {
-            port = Integer.parseInt(args[0])
+            port = Integer.parseInt(args[0]);
         }
 
         new DiscardServer(port).run();
