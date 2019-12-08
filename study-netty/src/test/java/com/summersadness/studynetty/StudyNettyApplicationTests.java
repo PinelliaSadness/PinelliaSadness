6bf55597b074a700646a72466a678420dfd7434e
liuyumao <PinelliaSadness@Gmail.com>
2019-12-08 16:33:46 +0800

    init

---
+++ study-netty/src/test/java/com/summersadness/studynetty/StudyNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summersadness.studynetty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyNettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
